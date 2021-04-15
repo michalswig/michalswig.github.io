0b694392a14e8e85c79db71ff8f423c0b0e8a4bd
Michał Świgost <mike@data.pl>
2021-04-15 13:35:09 +0200

    23.1 task Frontend edit6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://mighty-island-97230.herokuapp.com//v1/task/';
-   const trelloApiRoot = 'https://api.trello.com/1';
+   const apiRoot = 'http://localhost:8080/v1/task/';
+   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
